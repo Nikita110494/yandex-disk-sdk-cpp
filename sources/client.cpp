9e45af9270c3b5491ff55feeba431d49e4625388
Nikita110494 <komarov.05.11.94@mail.ru>
2017-03-07 17:36:57 +0300

    Update client.cpp

--- sources/client.cpp
+++ sources/client.cpp
@@ -16,39 +16,41 @@ namespace yadisk
 
 
     Client::Client(string token_) : token{token_} {}
-    auto upload(string url, url::path from, list<string> fields) -> json
+    auto Client::upload(string url, url::path from, std::list<string> fields) -> json
+
+  auto Client::upload(url::path to,string api_url, std::list<string> fields) -> json
 
   {
-    	CURL *curl;
- 	 CURLcode res;
- 	url::params_t url_params;
-		struct curl_slist *header_list = nullptr;
-		std::string auth_header;
-    
-    url_params["path"] = quote(to.string(), curl);
-    url_params["fields"] = boost::algorithm::join(fields, ",");
-		std::string url = api_url + "/copy?" + url_params.string();
-    auth_header = "Authorization: OAuth " + token;
-		header_list = curl_slist_append(header_list, auth_header.c_str()); 
-    stringstream response;
-  // Инициализация
-  curl_global_init(CURL_GLOBAL_ALL);
- 
-  curl = curl_easy_init();
+    		 CURL *curl;
+ 		
+ 	
+		 struct curl_slist *header_list = nullptr;
+		
+   		 url::params_t url_params;
+   		 url_params["path"] = quote(to.string(), curl);
+  		 url_params["fields"] = boost::algorithm::join(fields, ",");
+		 std::string url = api_url + "/copy?" + url_params.string();
+		 curl_slist * header_list = nullptr;
+  		 std::string auth_header = "Authorization: OAuth " + token;
+		 header_list = curl_slist_append(header_list, auth_header.c_str()); 
+                
+		
+ 		 stringstream result;
+ 		 curl = curl_easy_init();
    // Установка URL, который должен получить POST 
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  		  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     //Указываем данные POST
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=Nick&project=curl");
-      curl_easy_setopt(curl, CURLOPT_READDATA, &response);
-		curl_easy_setopt(curl, CURLOPT_READFUNCTION, write<stringstream>);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header_list);
+   
+     		 curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+		 curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write<stringstream>);
+		 curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+		 curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+		 curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header_list);
     //Начало выполнения запроса
-    auto res = curl_easy_perform(curl);
+   		 auto res = curl_easy_perform(curl);
     //Проверка на ошибки 
-   curl_slist_free_all(header_list);
-		curl_easy_cleanup(curl);
+  		 curl_slist_free_all(header_list);
+		 curl_easy_cleanup(curl);
 
 		if (res != CURLE_OK) return json();
 
@@ -57,7 +59,6 @@ namespace yadisk
   
 
 }
-=======
 	
 
     auto Client::ping() -> bool {
